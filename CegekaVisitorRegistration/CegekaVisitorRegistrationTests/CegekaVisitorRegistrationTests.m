ac5646ecdaf527b0b57c62e8cec5ee705ddb93ca
Jan Sabbe <jan.sabbe@gmail.com>
2013-06-18 22:17:20 +0200

    tests for core data

--- CegekaVisitorRegistration/CegekaVisitorRegistrationTests/CegekaVisitorRegistrationTests.m
+++ CegekaVisitorRegistration/CegekaVisitorRegistrationTests/CegekaVisitorRegistrationTests.m
@@ -7,6 +7,7 @@
 //
 
 #import "CegekaVisitorRegistrationTests.h"
+#import "CGKVisitor.h"
 
 @implementation CegekaVisitorRegistrationTests
 
@@ -17,12 +18,12 @@
     [super setUp];
     
     // Set-up code here.
-    NSBundle *bundle = [NSBundle bundleForClass:NSClassFromString(@"CGKVisitor")];
+    NSBundle *bundle = [NSBundle bundleForClass:[self class]];
     NSString* path = [bundle pathForResource:@"CGKVisitors" ofType:@"momd"];
     NSURL *momURL = [NSURL URLWithString:path];
     NSManagedObjectModel *model = [[NSManagedObjectModel alloc] initWithContentsOfURL:momURL];
     NSPersistentStoreCoordinator *coord = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel: model];
-    NSPersistentStore *store = [coord addPersistentStoreWithType:NSInMemoryStoreType configuration:nil URL:nil options:nil error:nil];
+    [coord addPersistentStoreWithType:NSInMemoryStoreType configuration:nil URL:nil options:nil error:nil];
     testingContext = [[NSManagedObjectContext alloc] init];
     [testingContext setPersistentStoreCoordinator: coord];
 }
@@ -34,9 +35,30 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testStoringAVisitor
 {
-    STFail(@"Unit tests are not implemented yet in CegekaVisitorRegistrationTests");
+    CGKVisitor* visitor = [CGKVisitor insertInManagedObjectContext:self.testingContext];
+    visitor.firstName = @"Name";
+    NSError* error = nil;
+    [self.testingContext save:&error];
+
+    STAssertNil(error, @"Error shouldn't occur while saving: %@", error);
+}
+
+- (void)testSearchingAVisitor
+{
+    CGKVisitor* visitor1 = [CGKVisitor insertInManagedObjectContext:self.testingContext];
+    visitor1.firstName = @"Name";
+    CGKVisitor* visitor2 = [CGKVisitor insertInManagedObjectContext:self.testingContext];
+    visitor2.firstName = @"Name2";
+    [self.testingContext save:nil];
+    
+    NSFetchRequest* request = [NSFetchRequest fetchRequestWithEntityName:[CGKVisitor entityName]];
+    request.predicate = [NSPredicate predicateWithFormat:@"firstName = %@", @"Name2"];
+    
+    NSArray* result = [self.testingContext executeFetchRequest:request error:nil];
+    STAssertEquals([result count], 1u, nil);
+    STAssertEqualObjects(result[0], visitor2, nil);
 }
 
 @end
