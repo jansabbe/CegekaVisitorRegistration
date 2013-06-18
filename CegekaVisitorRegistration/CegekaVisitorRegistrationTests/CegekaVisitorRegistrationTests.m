c9a2290b727b32cd2771543302e15ee834e65829
Werner Ramaekers <wernerramaekers@me.com>
2013-06-18 16:45:42 +0200

    first version

--- CegekaVisitorRegistration/CegekaVisitorRegistrationTests/CegekaVisitorRegistrationTests.m
+++ CegekaVisitorRegistration/CegekaVisitorRegistrationTests/CegekaVisitorRegistrationTests.m
@@ -10,11 +10,21 @@
 
 @implementation CegekaVisitorRegistrationTests
 
+@synthesize testingContext;
+
 - (void)setUp
 {
     [super setUp];
     
     // Set-up code here.
+    NSBundle *bundle = [NSBundle bundleForClass:NSClassFromString(@"CGKVisitor")];
+    NSString* path = [bundle pathForResource:@"CGKVisitors" ofType:@"momd"];
+    NSURL *momURL = [NSURL URLWithString:path];
+    NSManagedObjectModel *model = [[NSManagedObjectModel alloc] initWithContentsOfURL:momURL];
+    NSPersistentStoreCoordinator *coord = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel: model];
+    NSPersistentStore *store = [coord addPersistentStoreWithType:NSInMemoryStoreType configuration:nil URL:nil options:nil error:nil];
+    testingContext = [[NSManagedObjectContext alloc] init];
+    [testingContext setPersistentStoreCoordinator: coord];
 }
 
 - (void)tearDown
