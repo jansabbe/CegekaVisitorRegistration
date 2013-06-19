a088d6239fbb73602ff5de95d53bc2573d79a6ad
Werner Ramaekers <wernerramaekers@me.com>
2013-06-19 12:37:44 +0200

    Added WelcomeView to Storyboard
    WelcomeViewcontroller added
    Change Storyboard :
    -  Welcome to inputForm
    - Welcome to export

---
+++ CegekaVisitorRegistration/CegekaVisitorRegistration/CGKWelcomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  CGKWelcomeViewController.h
+//  CegekaVisitorRegistration
+//
+//  Created by Werner Ramaekers on 19/06/13.
+//  Copyright (c) 2013 Cegeka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CGKWelcomeViewController : UIViewController
+
+@end
---
+++ CegekaVisitorRegistration/CegekaVisitorRegistration/CGKWelcomeViewController.m
@@ -0,0 +1,38 @@
+//
+//  CGKWelcomeViewController.m
+//  CegekaVisitorRegistration
+//
+//  Created by Werner Ramaekers on 19/06/13.
+//  Copyright (c) 2013 Cegeka. All rights reserved.
+//
+
+#import "CGKWelcomeViewController.h"
+
+@interface CGKWelcomeViewController ()
+
+@end
+
+@implementation CGKWelcomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
